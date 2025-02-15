d86f93f0f2f0f6d913589c4b2a20017e759753ea
sysant <1549214302@qq.com>
2025-02-15 12:07:43 +0800

    add deploy 目录

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,8 +122,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    '/var/www/static/',
+    '/data/django-blog/static/',
 ]
+STATIC_ROOT = str(BASE_DIR / 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
